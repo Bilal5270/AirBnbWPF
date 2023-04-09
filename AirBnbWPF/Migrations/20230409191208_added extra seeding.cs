7a7cce519ff365f2e304e5daf48beb224ff71f97
Bilal Yousef <s1169284@student.windesheim.nl>
2023-04-09 21:13:13 +0200

    Added extra seeding

---
+++ AirBnbWPF/Migrations/20230409191208_added extra seeding.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirBnbWPF.Migrations
+{
+    public partial class addedextraseeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Landlords",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 3, "Atal", "Burhani" });
+
+            migrationBuilder.InsertData(
+                table: "Properties",
+                columns: new[] { "Id", "Address", "AmountOfRooms", "City", "LandlordId", "PostalCode", "PricePerNight" },
+                values: new object[,]
+                {
+                    { 3, "Rotterdamsestraat 21", 3, "Rotterdam", 2, "3011LL", 150 },
+                    { 4, "Runmolenstraat 46", 4, "Almere", 2, "1333AR", 40 },
+                    { 5, "De Wallen", 1, "Amsterdam", 2, "1063XO", 200 },
+                    { 6, "Prinsengracht 123", 2, "Amsterdam", 1, "1015DK", 100 },
+                    { 7, "Zeedijk 101", 3, "Amsterdam", 2, "1012AX", 120 },
+                    { 8, "Keizersgracht 456", 5, "Amsterdam", 1, "1017EG", 200 },
+                    { 9, "Groningerstraat 78", 4, "Groningen", 2, "9718PT", 80 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Properties",
+                columns: new[] { "Id", "Address", "AmountOfRooms", "City", "LandlordId", "PostalCode", "PricePerNight" },
+                values: new object[] { 10, "Vrijthof 55", 6, "Maastricht", 3, "6211LE", 250 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Landlords",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- AirBnbWPF/Model/AirBnbContext.cs
+++ AirBnbWPF/Model/AirBnbContext.cs
@@ -40,18 +40,27 @@ namespace AirBnbWPF.Model
             );
 
             modelBuilder.Entity<Property>().HasData(
-                new { Address = "Beeldhouwerpad 156 ", PostalCode = "1315KB", Id = 1, City = "Almere",  AmountOfRooms = 4, PricePerNight = 50, LandlordId = 1, ReservationId = 1 },
-                new { Address = "Kasteellaan 100", PostalCode = "8225LL", Id = 2, City = "Lelystad",  AmountOfRooms = 40, PricePerNight = 500 , LandlordId = 1}
+                new { Address = "Beeldhouwerpad 156 ", PostalCode = "1315KB", Id = 1, City = "Almere",  AmountOfRooms = 4, PricePerNight = 50, LandlordId = 1},
+                new { Address = "Kasteellaan 100", PostalCode = "8225LL", Id = 2, City = "Lelystad",  AmountOfRooms = 40, PricePerNight = 500 , LandlordId = 1},
+                new { Address = "Rotterdamsestraat 21", PostalCode = "3011LL", Id = 3, City = "Rotterdam", AmountOfRooms = 3, PricePerNight = 150, LandlordId = 2 },
+                new { Address = "Runmolenstraat 46", PostalCode = "1333AR", Id = 4, City = "Almere", AmountOfRooms = 4, PricePerNight = 40, LandlordId = 2 },
+                new { Address = "De Wallen", PostalCode = "1063XO", Id = 5, City = "Amsterdam", AmountOfRooms = 1, PricePerNight = 200, LandlordId = 2},
+                new { Address = "Prinsengracht 123", PostalCode = "1015DK", Id = 6, City = "Amsterdam", AmountOfRooms = 2, PricePerNight = 100, LandlordId = 1 },
+                new { Address = "Zeedijk 101", PostalCode = "1012AX", Id = 7, City = "Amsterdam", AmountOfRooms = 3, PricePerNight = 120, LandlordId = 2 },
+                new { Address = "Keizersgracht 456", PostalCode = "1017EG", Id = 8, City = "Amsterdam", AmountOfRooms = 5, PricePerNight = 200, LandlordId = 1 },
+                new { Address = "Groningerstraat 78", PostalCode = "9718PT", Id = 9, City = "Groningen", AmountOfRooms = 4, PricePerNight = 80, LandlordId = 2 },
+                new { Address = "Vrijthof 55", PostalCode = "6211LE", Id = 10, City = "Maastricht", AmountOfRooms = 6, PricePerNight = 250, LandlordId = 3 }
             );
 
             modelBuilder.Entity<Landlord>().HasData(
                 new { FirstName = "Karim", LastName = "Ayachi", Id = 1},
-                new { FirstName = "Yousry", LastName = "Yousef", Id = 2 }
+                new { FirstName = "Yousry", LastName = "Yousef", Id = 2 },
+                new { FirstName = "Atal", LastName = "Burhani", Id = 3}
 
             );
 
             modelBuilder.Entity<Reservation>().HasData(
-               new  { Id = 1, StartDate = new DateTime (2022, 6, 1), EndDate = new DateTime(2022, 7, 1), UserId = 1 , PropertyId = 1 },
+               new { Id = 1, StartDate = new DateTime (2022, 6, 1), EndDate = new DateTime(2022, 7, 1), UserId = 1 , PropertyId = 1 },
                new { Id = 2, StartDate = new DateTime(2022, 3, 7), EndDate = new DateTime(2022, 3, 8), UserId = 2, PropertyId = 2 }
 
            );
